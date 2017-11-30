9d1876b65e4bb70d1d872b3fcec6df62f5e45a41
a-del <32435412+a-del@users.noreply.github.com>
2017-11-30 19:34:24 +0100

    added methods for data analysis

--- Universe.py
+++ Universe.py
@@ -46,4 +46,64 @@ class Universe:
             k += 8
         temp=a>>k
         
+        
+    ## functions for analysis
+    def colonisationRate(self) :
+        "renvoie le taux de cases remplies dans la liste Memoire[] de l'univers à la date t"
+        instructionsNb = 0;
+        for i in self.Memory :
+            if i != None :
+                instructionsNb += 1;
+        return (instructionsNb/len(self.Memory)) ;
+
+
+    def individus_boucles(t) : # pas fini du tout !
+        "??? en utilisant la méthose donnée par le tuteur : un CPU-test parcourt le code, on identifie un individu-boucle comme la plus grande boucle possible."
+        # je ne sais plus s'il faut pouvoir retourner au début depuis la fin, où juste arriver à la fin depuis le début
+        self = reconstitue(t);
+        N = len(self.Memoire);   # devrait être une variable globale ?
+        loopStart = [None] * N;
+        test = CPU() ;
+        for i in range(N) :
+            if loopStart[i] == None :
+                loopStart[i] = i;
+                debut_courant = i;
+                test.index = i;
+                while ?? :
+                    test.execute();
+                    if ?? :
+                        debut_boucle[test.index] = debut_courant;
+                test.clear() ; #si cette méthode n'existe pas, on peut juste remplacer par test=CPU();
+        return debut_boucle;
+
+
+    def rateCreationCPUs(self, interval) :
+        "retourne le nombre moyen de CPUs créés par CPU existant et par an pendant l'intervalle de temps de longueur interval et commençant à l'état présent"
+        nbCreated = 0;
+        date = 0;
+        currentCPUnumber = len(self.CPU);
+        while date < interval :
+            self.roundSlicer() ;
+            nbCreated += len(self.CPU)/currentCPUnumber - 1 ; # ou faire une fonction qui calcule le nb de CPUs crées à la date t ?
+            currentCPUnumber = len(self.CPU);
+        return (nb_created/interval) ;
+
+
+    def CPUdensity(self, t) :
+        "affiche et renvoie la densite de CPU par site d'instruction occupé" #on pourrait faire par site d'instruction tout court ?
+        d = len(self.CPU) / (self.colonisationRate()*len(self.Memory)) ;
+        print("La densité de CPU est {}, soit 1 CPU pour {} instructions".format(d, 1/d));
+        return d;
+
+
+    def nbIndividuals(t) :
+        "renvoie le nombre d'individus-boucles"
+
+    def nbSpecies(t) :
+        "renvoie le nombre de codes différents parmi tous les individus-boucles identifiés"
+
+    def speciesDistance(??) :
+        "renvoie la distance d'édition entre les codes des deux espèces données en paramètre"
+
+        
     
